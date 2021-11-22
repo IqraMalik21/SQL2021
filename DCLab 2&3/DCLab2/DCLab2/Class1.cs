2165c200fd31852b924d2af7b243618bb2e83fbf
IqraMalik21 <54285813+IqraMalik21@users.noreply.github.com>
2021-11-22 03:23:46 -0800

    DC Lab 2&3 (Tasks Done)
    Select, Join (Using WHERE), Insert, Update, Delete Done

---
+++ DCLab 2&3/DCLab2/DCLab2/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+
+
+namespace DCLab2
+{
+    class DataFunctions
+    {
+        public DataTable getData(string SQL)
+        {
+            DataTable DT = new DataTable();
+            string conStr = "Data Source=DESKTOP-9FMS3P9\\MSSQLSERVER01;Initial Catalog=DCNewDatabase1;Integrated Security=True";
+            SqlConnection ConObj = new SqlConnection(conStr);
+            ConObj.Open();
+            SqlDataAdapter da = new SqlDataAdapter(SQL, ConObj);
+            da.Fill(DT);
+            ConObj.Close();
+            return DT;
+        }
+
+        public DataTable Manipulation(string SQL)
+        {
+            DataTable DT = new DataTable();
+            string conStr = "Data Source=DESKTOP-9FMS3P9\\MSSQLSERVER01;Initial Catalog=DCNewDatabase1;Integrated Security=True";
+            SqlConnection ConObj = new SqlConnection(conStr);
+            ConObj.Open();
+            SqlCommand com = new SqlCommand();
+            com.Connection = ConObj;
+            com.CommandText = SQL;
+            com.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Student;", ConObj);
+            da.Fill(DT);
+            ConObj.Close();
+            return DT;
+
+        }
+        public bool InsertData(string SQL_Insert)
+        {
+            string conStr = "Data Source = DESKTOP - 9FMS3P9\\MSSQLSERVER01; Initial Catalog = DCNewDatabase1; Integrated Security = True";
+            SqlConnection ConObj = new SqlConnection(conStr);
+            ConObj.Open();
+            SqlCommand cmd = new SqlCommand(SQL_Insert, ConObj);
+            int rowsAffected = 0;
+            rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected > 0)
+            {
+                return true;
+
+            }
+            else return false;
+        }
+
+    }
+}
---
+++ DCLab 2&3/DCLab2/DCLab2/Form1.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new DCLab2.DataFunctions();
+            string SQL_Slect = "SELECT * FROM Student; ";
+            dataGridView1.DataSource = dataObj.getData(SQL_Slect);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new DCLab2.DataFunctions();
+            string SQL_Slect = "SELECT Student.[StudentRollNo], Student.Name, Student.Batch, Student.[FatherName], Student.Year, City.CityName FROM Student, City WHERE Student.CityID = City.CityID;";
+            dataGridView1.DataSource = dataObj.getData(SQL_Slect);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new DCLab2.DataFunctions();
+            string SQL_Insert = "INSERT INTO Student (StudentRollNo, Name, FatherName, Batch, Year, CityID) VALUES (" + int.Parse(textBox1.Text) + ",'" + textBox2.Text + "','" + textBox3.Text + "'," + int.Parse(textBox4.Text) + "," + int.Parse(textBox5.Text) + "," + (comboBox1.SelectedIndex + 1) + "); ";
+            dataGridView1.DataSource = dataObj.Manipulation(SQL_Insert);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new DCLab2.DataFunctions();
+            string SQL_Update = "UPDATE Student SET Name= ' " + textBox2.Text + " ' , FatherName= ' " + textBox3.Text + " ' , Batch= " + int.Parse(textBox4.Text) + " , Year= " + int.Parse(textBox5.Text) + " WHERE StudentRollNo= " + int.Parse(textBox1.Text) + "," + (comboBox1.SelectedIndex + 1) + "; ";
+            dataGridView1.DataSource = dataObj.Manipulation(SQL_Update);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DataFunctions dataObj = new DCLab2.DataFunctions();
+            string SQL_Delete = "DELETE FROM Student WHERE StudentRollNo= " + int.Parse(textBox1.Text) + ";";
+            dataGridView1.DataSource = dataObj.Manipulation(SQL_Delete);
+        }
+    }
+}
